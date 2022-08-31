073aa8fcef22f086fee226fc4590bffb5c50c63b
Ashton MacKenzie <pikeminnow@gmail.com>
2022-08-31 12:41:51 -0400

    commit tree c3cf4e978b089d49718afc564c86b0e8c32770a5

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    debugger;
+    console.log("The DOM has loaded");
+    document.getElementById("text").textContent = "This is really cool!";
+    //document.write(document.getElementById("text").textcontent = "This is really cool!");
+    debugger;
+});
